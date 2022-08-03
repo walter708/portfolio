27b8d55f0a1bb52d28c623d6256faf09d58ccd7f
walter708 <uchewalter8@gmail.com>
2022-08-03 17:52:11 -0400

    okpala:added the social media and navigation dots

--- portfolio_frontend/src/components/Navbar/Navbar.jsx
+++ portfolio_frontend/src/components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
       <HiMenuAlt4 onClick={() => setToggle(true)} />
       
       {toggle && (
-        <motion.div
+        <motion.div 
         whileInView={{x: [300, 0]}}
         transition = {{duration:0.85, ease:'easeOut'}}
         >
