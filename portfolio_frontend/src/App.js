07b7a2aec47d8dc9d9d65778e3f2b7fdba605f50
walter708 <uchewalter8@gmail.com>
2022-08-03 12:56:26 -0400

    okpala:started the navbar

---
+++ portfolio_frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.scss'
+import { About, Footer, Header,Skills, Testimonials, Work  } from './container'
+import {Navbar} from './components'
+const App = () => {
+  return (
+    <Navbar />
+  )
+}
+
+export default App 
